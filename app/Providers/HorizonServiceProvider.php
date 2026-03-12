da524ce2d348af9cc1c3461c013e56184527ba53
Kawsar Ahmed <kawsarapps72@gmail.com>
2026-03-13 02:28:34 +0600

    Theme Inded design update

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -25,12 +25,19 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
      *
      * This gate determines who can access Horizon in non-local environments.
      */
+
+
     protected function gate(): void
     {
-        Gate::define('viewHorizon', function ($user = null) {
-            return in_array(optional($user)->email, [
-                //
-            ]);
+        Gate::define('viewHorizon', function ($user) {
+            // Ekhane apnar system er admin (User ID 1) ke allow kore din
+            return $user->id === 1; 
+            
+            // Athoba apni chaile email diyeo allow korte paren:
+            // return in_array($user->email, [
+            //     'admin@asianhost.net',
+            // ]);
         });
     }
+
 }
